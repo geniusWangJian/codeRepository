2cc8300ab1ba29a9932d9aad68fcceaf32f28df3
geniusWangJian <geniuswangjian@gmail.com>
2020-03-19 17:28:03 +0800

    新的

---
+++ first_project/src/main/java/mobi/wangjian/first_project/App.java
@@ -0,0 +1,13 @@
+package mobi.wangjian.first_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
